2d72edf056fab8708023c3b96c96eb9afa5c6056
KipkorirNovestus <kipkorirnovestus1@gmail.com>
2024-06-06 21:26:58 +0300

    changed some titles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-
-  const Home = () => <h1>Welcome to my Blog</h1>
-  return ( 
+  const Home = () => <h1>Welcome to my Blog</h1>;
+  return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About />}/>
-      
-    </Routes>
-    
-    </BrowserRouter>
-
-
-
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          git{" "}
+        </Routes>
+      </BrowserRouter>
     </>
-   );
+  );
 }
- 
+
 export default App;
